185cca221ead6012682ed0ebbdf6b39a7ee8b4b8
Pavel Keyzik <pavel.keyzik@gmail.com>
2023-09-21 23:36:14 +0200

    feat: add Button component styles

---
+++ src/components/UnstyledWithTheme.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { Unstyled } from "@storybook/blocks";
+import { PropsWithChildren } from "react";
+
+type Props = PropsWithChildren
+
+function UnstyledWithTheme(props: Props) {
+	function renderChild() {
+		return (
+			<div style={{ padding: 24, border: "2px solid #eee", background: "var(--body-background)" }}>
+				{props.children}
+			</div>
+		);
+	}
+
+	function renderHeader(title: string) {
+		return (
+			<div style={{ marginBottom: "12px", fontWeight: "bold" }}>{title}</div>
+		);
+	}
+
+	return (
+		<Unstyled>
+			<div style={{display: "grid", gap: 16, gridTemplateColumns: "1fr 1fr"}}>
+				<div className="light-theme">
+					{renderHeader("Light Theme")}
+					{renderChild()}
+				</div>
+				<div className="dark-theme">
+					{renderHeader("Dark Theme")}
+					{renderChild()}
+				</div>
+			</div>
+		</Unstyled>
+	);
+}
+
+export { UnstyledWithTheme };
